0d6ede3b311d0910437b64b328c82d0de87eb38d
RSR Technologies <143055765+RameshSP-gif@users.noreply.github.com>
2025-07-20 23:14:03 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = ({ toggleSidebar }) => {
             <img
               src={logo}
               alt="MCC Bank Logo"
-              style={{ height: "10px", marginRight: "5px" }}
+              style={{ height: "5px", marginRight: "5px" }}
             />
             <span className="fw-bold fs-5">MCC Bank System</span>
           </Link>
